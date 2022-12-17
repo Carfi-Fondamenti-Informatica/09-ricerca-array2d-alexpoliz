879a4c17088c34e74816484de02badde6b508642
alexpoliz <117836399+alexpoliz@users.noreply.github.com>
2022-12-17 01:06:29 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,5 +1,5 @@
 
-int funzione (char lista[10][20], char nome[20]) {
+int funzione (char lista[][20], char nome[20]) {
     int i=0; int j=0;
     while ((j<19)and(i<10)) {
         if (lista[i][j]!=nome[j]) {
